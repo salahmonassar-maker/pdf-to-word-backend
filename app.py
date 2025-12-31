4f4ab5a15138f5399547fda6257f0c93efd02d21
salahmonassar-maker <salahmonassar@gmail.com>
2025-12-31 22:03:07 +0300

    Configure app to run on specified port

--- app.py
+++ app.py
@@ -15,4 +15,8 @@ def convert():
     return send_file("output.docx", as_attachment=True)
 
 if __name__ == "__main__":
-    app.run()
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
